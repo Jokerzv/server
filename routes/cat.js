8e4a43950a69f5a04952190606d2db1ab86611b5
Jokerzv <jokerobscure@gmail.com>
2018-08-30 12:10:24 +0300

    update

--- routes/cat.js
+++ routes/cat.js
@@ -125,6 +125,33 @@ function getscat(){
       });
 
     });
+
+  }else if(status == "down"){
+  console.log("STATUS DOWN ");
+
+    Cat.find({position: one_p}, function(err, results){
+      if(err) return console.log(err);
+      //console.log(results);
+      console.log("ID ONE ", results);
+      Cat.updateOne({_id: id_one}, {position: one_p}, function(err, docs){
+        if(err) return console.log(err);
+        console.log("res ", docs);
+        //console.log("Update position one!");
+          });
+
+  });
+
+  Cat.find({position: two_p}, function(err, results){
+      if(err) return console.log(err);
+console.log("ID TWO ", results);
+      Cat.updateOne({_id: id_two}, {position: two_p}, function(err, docs){
+        if(err) return console.log(err);
+
+        //console.log("Update position two!");
+      });
+
+    });
+
   }
 
 }
@@ -184,6 +211,109 @@ function getscatup(){
     });
 }
 
+function getscatdown(){
+  var count_cats_n = 0;
+  User.find({token: req.query.token}).count(function(err, results){
+
+    if(err) return console.log(err);
+    if(results > 0){
+      User.find({token: req.query.token, verif: 1}, function(err, user_data){
+        if(err) return console.log(err);
+
+        Cat.find({user_id: user_data[0]._id}).count(function(err, count_cats_now){
+            if(err) return console.log(err);
+            count_cats_n = count_cats_now - 1;
+          console.log("COUNT CAT: ", count_cats_now);
+        });
+         Cat.find({user_id: user_data[0]._id, _id: req.query.catid}).count(function(err, results){
+
+          if(results > 0){
+
+            Cat.find({_id: req.query.catid}, function(err, user_data_cat){
+              console.log("OK find user");
+              if(err) return console.log(err);
+
+              console.log(user_data_cat[0].position);
+              if(user_data_cat[0].position != count_cats_n){
+                var one = user_data_cat[0].position,
+                    two = user_data_cat[0].position + 1;
+
+                Cat.find({position: [one, two]}, function(err, cats_find){
+                  console.log("send func up_pos");
+                  //  User.update({token: md5(now().toString())}, function(err, docs){
+                        //console.log("ID one  ", cats_find[0]._id, "id two ", cats_find[1]._id);
+                        update_cats_p(one, cats_find[0]._id, two, cats_find[1]._id, "down");
+                  //  });
+
+
+                });
+
+              }
+              //console.log("Not find user: ",catres);
+              catres_data = [{
+                status: "отвечаю cat!"
+              }]
+              res.send(catres_data);
+
+
+               });
+
+          }else{
+            res.send([]);
+            console.log("Not cat user: ",results);
+          }
+
+            });
+       });
+
+    }else{
+      res.send({status: "not find user"});
+      console.log("Not find user: ",results);
+    }
+    });
+}
+
+function getscatdelete(){
+
+  User.find({token: req.query.token}).count(function(err, results){
+
+    if(err) return console.log(err);
+    if(results > 0){
+      User.find({token: req.query.token, verif: 1}, function(err, user_data){
+        if(err) return console.log(err);
+
+
+         Cat.find({user_id: user_data[0]._id, _id: req.query.catid}).count(function(err, results){
+
+          if(results > 0){
+
+            Cat.deleteOne({_id: req.query.catid}, function(err, user_data_cat){
+              console.log("OK find user");
+              if(err) return console.log(err);
+
+              //console.log("Not find user: ",catres);
+              catres_data = [{
+                status: "отвечаю cat!"
+              }]
+              res.send(catres_data);
+
+
+               });
+
+          }else{
+            res.send([]);
+            console.log("Not cat user: ",results);
+          }
+
+            });
+       });
+
+    }else{
+      res.send({status: "not find user"});
+      console.log("Not find user: ",results);
+    }
+    });
+}
      if(req.query.status == "add"){
 
         User.find({token: req.query.token, verif: 1}).count(function(err, results){
@@ -221,6 +351,12 @@ function getscatup(){
      }else if(req.query.status == "getcatup"){
 
         getscatup();
+     }else if(req.query.status == "getscatdown"){
+
+        getscatdown();
+     }else if(req.query.status == "getscatdelete"){
+
+        getscatdelete();
      }else if(req.query.status == "signup"){
 
 
